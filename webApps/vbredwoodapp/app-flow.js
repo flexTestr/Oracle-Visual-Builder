6aaa03bf111afff5517588ef29792d07e3786d9f
flexTestr <surya.pixel6a@gmail.com>
2025-05-20 20:22:29 +0530

    Delete webApps directory

--- webApps/vbredwoodapp/app-flow.js
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2023, Oracle and/or its affiliates */
-
-define(['fndconfig/config'], function() {
-  'use strict';
-
-  var AppModule = function AppModule() {};
-
-  return AppModule;
-});
--- webApps/vbredwoodapp/flows/welcome/welcome-flow.js
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2023, Oracle and/or its affiliates */
-
-define([], function() {
-  'use strict';
-
-  var FlowModule = function FlowModule() {};
-
-  return FlowModule;
-});
